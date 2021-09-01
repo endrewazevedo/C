996a4f27a363604b32bde11dea997bf514781768
Maykin21 <maycon.expert21@gmail.com>
2021-09-01 19:08:24 -0400

    commit

---
+++ LAB 4 - Funções e Ponteiros/media_situacao.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+char situacao(float p1, float p2, float p3, int faltas, int aulas, float *media){
+	float porcent_faltas = ( (float) aulas/100)*25;
+	char sit_aluno;
+	float media_final = (p1+p2+p3)/3;
+	*media = media_final;
+	if(faltas > porcent_faltas){
+		sit_aluno = 'F';
+		return sit_aluno;
+	}
+	else if(*media < 6.0){
+		sit_aluno = 'R';
+		return sit_aluno;
+	}
+	else{
+	    sit_aluno = 'A';
+		return sit_aluno; 
+	}
+}
+
+int main(){
+	float p1, p2, p3, media;
+	int faltas, aulas;
+	scanf("%f %f %f %d %d", &p1, &p2, &p3, &faltas, &aulas);
+	situacao(p1, p2, p3, faltas, aulas, &media);
+	printf("Media: %.1f, Situacao: %c ", media, situacao(p1, p2, p3, faltas, aulas, &media));
+}
